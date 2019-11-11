83c9d8a96033c6d6fb2266434e20b88f3b1d438b
Tayu0404 <tkt.yusei0404@gmail.com>
2019-11-11 13:18:57 +0900

    Update MainForm Design

--- src/Other Resources Encoder/Other Resources Encoder/Form/MainForm.Design.cs
+++ src/Other Resources Encoder/Other Resources Encoder/Form/MainForm.Design.cs
@@ -4,25 +4,25 @@ using System.Windows.Forms;
 using AxMSVidCtlLib;
 
 partial	class MainForm : Form {
-	private	void InitializeComponent(){
+	private void InitializeComponent() {
 		int margen = 15;
 		int curW, curH;
 
 
 		//Encode Setting
-		this.encodeLabel                  = new Label();
-		this.outPutFileNameLabel          = new Label();
-		this.outPutFileName               = new TextBox();
-		this.resourceMachine              = new ComboBox();
+		this.encodeLabel = new Label();
+		this.outPutFileNameLabel = new Label();
+		this.outPutFileName = new TextBox();
+		this.resourceMachine = new ComboBox();
 		this.resourceMachineSettingButton = new Button();
-		this.encodeProfile                = new ComboBox();
-		this.encodeProfileSaveButton      = new Button();
-		this.encoderLabel                 = new Label();
-		this.encoder                      = new ComboBox();
-		this.outPutVideoCheckBox          = new CheckBox();
-		this.outPutVideoLabel             = new Label();
-		this.outPutAudioCheckBox          = new CheckBox();
-		this.outPutAudioLabel             = new Label();
+		this.encodeProfile = new ComboBox();
+		this.encodeProfileSaveButton = new Button();
+		this.encoderLabel = new Label();
+		this.encoder = new ComboBox();
+		this.outPutVideoCheckBox = new CheckBox();
+		this.outPutVideoLabel = new Label();
+		this.outPutAudioCheckBox = new CheckBox();
+		this.outPutAudioLabel = new Label();
 
 		//Video Preview
 		this.videoPreview = new AxMSVidCtl();
@@ -30,53 +30,63 @@ partial	class MainForm : Form {
 		curW = curH = margen;
 
 		this.encodeLabel.Text = "Encode";
-		this.encodeLabel.Size = new Size(400, 30);
+		this.encodeLabel.Font = new Font("arial", 15f);
+		this.encodeLabel.Size = new Size(400, 25);
 		this.encodeLabel.Location = new Point(curW, curH);
 
 		this.videoPreview.Size = new Size(960, 540);
 		this.videoPreview.Location = new Point(curW + this.encodeLabel.Size.Width + margen, curH);
 
-		curH += this.encodeLabel.Size.Height;
+		curH += this.encodeLabel.Size.Height + margen;
 
 		this.outPutFileNameLabel.Text = "OutPut File Name :";
-		this.outPutFileNameLabel.Size = new Size(70, 20);
+		this.outPutFileNameLabel.Font = new Font("arial", 12f);
+		this.outPutFileNameLabel.TextAlign = ContentAlignment.MiddleLeft;
+		this.outPutFileNameLabel.Size = new Size(140, 25);
 		this.outPutFileNameLabel.Location = new Point(curW, curH);
 
-		this.outPutFileName.Size = new Size(330, 20);
+		this.outPutFileName.Size = new Size(260, 25);
+		this.outPutFileName.Font = new Font("arial", 12f);
 		this.outPutFileName.Location = new Point(curW + this.outPutFileNameLabel.Size.Width, curH);
 
-		curH += this.outPutFileName.Size.Height;
+		curH += this.outPutFileNameLabel.Size.Height + margen;
 
 		this.resourceMachine.Items.AddRange(new object[] {
 			"Resource Machine"
 		});
 		this.resourceMachine.SelectedIndex = 0;
 		this.resourceMachine.DropDownStyle = ComboBoxStyle.DropDownList;
-		this.resourceMachine.Size = new Size(350, 20);
+		this.resourceMachine.Font = new Font("arial", 12f);
+		this.resourceMachine.Size = new Size(300,0);
 		this.resourceMachine.Location = new Point(curW, curH);
 
 		this.resourceMachineSettingButton.Text = "Setting";
-		this.resourceMachineSettingButton.Size = new Size(50, 20);
+		this.resourceMachineSettingButton.Font = new Font("arial", 12f);
+		this.resourceMachineSettingButton.Size = new Size(100, this.resourceMachine.Size.Height);
 		this.resourceMachineSettingButton.Location = new Point(curW + this.resourceMachine.Size.Width, curH);
 
-		curH += this.resourceMachineSettingButton.Size.Height;
+		curH += this.resourceMachineSettingButton.Size.Height + margen;
 
 		this.encodeProfile.Items.AddRange(new object[] {
 			"Profile"
 		});
 		this.encodeProfile.SelectedIndex = 0;
 		this.encodeProfile.DropDownStyle = ComboBoxStyle.DropDownList;
-		this.encodeProfile.Size = new Size(350, 20);
+		this.encodeProfile.Font = new Font("arial", 12f);
+		this.encodeProfile.Size = new Size(300, 0);
 		this.encodeProfile.Location = new Point(curW, curH);
 
 		this.encodeProfileSaveButton.Text = "Save";
-		this.encodeProfileSaveButton.Size = new Size(50, 20);
+		this.encodeProfileSaveButton.Font = new Font("arial", 12f);
+		this.encodeProfileSaveButton.Size = new Size(100, this.encodeProfile.Size.Height);
 		this.encodeProfileSaveButton.Location = new Point(curW + this.encodeProfile.Size.Width, curH);
 
-		curH = curH + this.encodeProfileSaveButton.Size.Height;
+		curH += this.encodeProfileSaveButton.Size.Height + margen;
 
 		this.encoderLabel.Text = "Encoder";
-		this.encoderLabel.Size = new Size(50, 20);
+		this.encoderLabel.Font = new Font("arial", 12f);
+		this.encoderLabel.Size = new Size(100, 30);
+		this.encoderLabel.TextAlign = ContentAlignment.MiddleLeft;
 		this.encoderLabel.Location = new Point(curW, curH);
 
 		this.encoder.Items.AddRange(new object[]{
@@ -85,10 +95,11 @@ partial	class MainForm : Form {
 		});
 		this.encoder.SelectedIndex = 0;
 		this.encoder.DropDownStyle = ComboBoxStyle.DropDownList;
-		this.encoder.Size = new Size(350, 20);
+		this.encoder.Font = new Font("arial", 12f);
+		this.encoder.Size = new Size(300, 0);
 		this.encoder.Location = new Point(curW + this.encoderLabel.Size.Width, curH);
 
-		curH = curH + this.encoder.Size.Height;
+		curH += this.encoder.Size.Height + margen;
 
 		int outputSettingsWidth = curW;
 
@@ -99,19 +110,22 @@ partial	class MainForm : Form {
 		outputSettingsWidth += outPutVideoCheckBox.Size.Width;
 
 		this.outPutVideoLabel.Text = "Output Video";
-		this.outPutVideoLabel.Size = new Size(180, 20);
+		this.outPutVideoLabel.Font = new Font("arial", 12f);
+		this.outPutVideoLabel.Size = new Size(180, 30);
 		this.outPutVideoLabel.Location = new Point(outputSettingsWidth, curH);
 
 		outputSettingsWidth += this.outPutVideoLabel.Size.Width;
 
 		this.outPutAudioCheckBox.Checked = true;
+		this.outPutAudioCheckBox.Font = new Font("arial", 12f);
 		this.outPutAudioCheckBox.Size = new Size(20, 20);
 		this.outPutAudioCheckBox.Location = new Point(outputSettingsWidth, curH);
 
 		outputSettingsWidth += this.outPutAudioCheckBox.Size.Width;
 
 		this.outPutAudioLabel.Text = "Output Audio";
-		this.outPutAudioLabel.Size = new Size(180, 20);
+		this.outPutAudioLabel.Font = new Font("arial", 12f);
+		this.outPutAudioLabel.Size = new Size(180, 30);
 		this.outPutAudioLabel.Location = new Point(outputSettingsWidth, curH);
 
 		this.Text = "Other Resource Encoder";
