71d8889820873f93b9423b8f7134bf30cbce54b7
Tayu0404 <tkt.yusei0404@gmail.com>
2019-11-13 12:18:04 +0900

    Add SettingForm and SettingForm.Design

---
+++ src/Other Resources Encoder/Other Resources Encoder/Form/SettingForm.Design.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+partial class SettingForm : Form {
+	private void InitializeComponent() {
+	}
+}
---
+++ src/Other Resources Encoder/Other Resources Encoder/Form/SettingForm.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+partial class SettingForm : Form {
+	public SettingForm() {
+		InitializeComponent();
+	}
+}
