539420f73239a8deda6b1d123cceb000e7c917dd
Tayu0404 <tkt.yusei0404@gmail.com>
2019-11-14 12:00:47 +0900

    Add variables to SettingForm

--- src/Other Resources Encoder/Other Resources Encoder/Form/SettingForm.cs
+++ src/Other Resources Encoder/Other Resources Encoder/Form/SettingForm.cs
@@ -3,6 +3,18 @@ using System.Drawing;
 using System.Windows.Forms;
 
 partial class SettingForm : Form {
+	ListBox optionSettings;
+	//Remote Machine
+	Label remoteMachineLabel, profileLabel, hostLabel, portLabel, userLabel, passLabel;
+	ComboBox machineProfile;
+	TextBox profileName, host, port, user, password;
+	Button profileSave;
+	//SSH Key
+	Label sshKeyLabel;
+	Button privateKeyCopy, privateKeyOpen, publicKeyCopy, publicKeyOpen, keygen;
+	//Priority
+	Label usagePriority;
+	
 	public SettingForm() {
 		InitializeComponent();
 	}
