e3437b9d29e3821f310d9aa16dcb91a0fda87b2e
Tayu0404 <tkt.yusei0404@gmail.com>
2019-11-26 14:54:35 +0900

    Update SSH class

--- src/Other Resources Encoder/Other Resources Encoder/SSH/ssh.cs
+++ src/Other Resources Encoder/Other Resources Encoder/SSH/ssh.cs
@@ -2,7 +2,38 @@
 using Renci.SshNet;
 
 partial class SSH {
-	public SSH(){
+	private string host;
+	private string hostname;
+	private string port;
+	private string user;
+	private string password;
+	private string identityfile;
+	public SSH() {
+		
+	}
 
+	public string Host {
+		set { value = host; }
+		get { return host; }
+	}
+	public string HostName {
+		set { value = hostname; }
+		get { return hostname; }
+	}
+	public string Port {
+		set { value = port; }
+		get { return port; }
+	}
+	public string User {
+		set { value = user; }
+		get { return user; }
+	}
+	public string Password {
+		set { value = password; }
+		get { return password; }
+	}
+	public string Identityfile {
+		set { value = identityfile; }
+		get { return identityfile; }
 	}
 }
